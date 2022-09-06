15dff932cbdeed5cb4564b0d638ce96e5d9fef89
Haley Hickey <hchickey96@gmail.com>
2022-09-06 14:40:20 -0500

    creativeSmack

--- CreativeSmackTalkingPlayer.cs
+++ CreativeSmackTalkingPlayer.cs
@@ -1,8 +1,24 @@
+using System;
+using System.Collections.Generic;
+
 namespace ShootingDice
 {
     // A SmackTalkingPlayer who randomly selects a taunt from a list to say to the other player
-    public class CreativeSmackTalkingPlayer
+    public class CreativeSmackTalkingPlayer : Player
     {
+        public List<string> Taunt = new List<string>()
+        {
+            "You are bad at dice", "Learn how to roll better", "You have a bad wrist"
+        };
 
+        public override int Roll()
+        {
+            int randomIndex = new Random().Next(Taunt.Count);
+            string randomTaunt = Taunt[randomIndex];
+
+            Console.WriteLine(randomTaunt);
+
+            return new Random().Next(DiceSize) + 1;
+        }
     }
 }
